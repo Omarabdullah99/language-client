67d857af6dcf9c1a8e836f3b999d579e8be4f61a
Omarabdullah99 <omarabdullah917303@gmail.com>
2024-12-15 12:07:18 +0600

    update protectedUser

--- src/protectedRoute/ProtectedUser.jsx
+++ src/protectedRoute/ProtectedUser.jsx
@@ -3,7 +3,8 @@ import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 
 const ProtectedUser = ({children}) => {
-    const{user}=useSelector((state)=>({...state.authentication}))
+    // const{user}=useSelector((state)=>({...state.authentication}))
+    const user = JSON.parse(localStorage.getItem("blogprofile"));
     if(!user?.token){
         return <Navigate to={'/login'} replace="true"></Navigate>
     }
